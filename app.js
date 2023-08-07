99ef54095d7e9ecd1740eefb533f3aab8d104416
Yuanzhe-Li <li.yuanzhe@northeastern.edu>
2023-08-07 15:59:26 -0700

    a5 REST su23

---
+++ app.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cors from "cors";
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import AuthController from "./controllers/users/auth-controller.js";
+import session from "express-session";
+import "dotenv/config"
+
+const app = express()
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    })
+)
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+
+
+app.use(
+    session(sessionOptions)
+)
+
+app.use(express.json())
+const port = process.env.PORT || 4000
+
+AuthController(app)
+TuitsController(app)
+HelloController(app)
+UserController(app)
+
+app.listen(port);
