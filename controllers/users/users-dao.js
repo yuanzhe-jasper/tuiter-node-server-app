6db8ed3aeac0f14acc881d06b62471d8c03b1d07
Yuanzhe-Li <li.yuanzhe@northeastern.edu>
2023-08-13 10:21:47 -0700

    a6 Mongo su23

--- controllers/users/users-dao.js
+++ controllers/users/users-dao.js
@@ -1,42 +1,19 @@
-let users = [];
-export const findAllUsers = () => users;
-
-export const findUserById = (uid) => {
-  const index = users.findIndex((u) => u._id === uid);
-  if (index !== -1) return users[index];
-  return null;
-};
-
-export const findUserByUsername = (username) => {
-  const index = users.findIndex((u) => u.username === username);
-  if (index !== -1) return users[index];
-  return null;
-};
-
-export const findUserByCredentials = (username, password) => {
-  const index = users.findIndex(
-    (u) => u.username === username && u.password === password
-  );
-  if (index !== -1) return users[index];
-  return null;
-};
-
-export const createUser = (user) => {
-  users.push(user);
-  console.log(user)
-}
+import usersModel from "./users-model.js";
 
 
 
-export const updateUser = (uid, user) => {
-  const index = users.findIndex((u) => u._id === uid);
-  users[index] = { ...users[index], ...user };
-  return { status: "ok", user: users[index] };
-};
+export const findAllUsers = () => usersModel.find();
 
+export const findUserById = (id) => usersModel.findById(id);
 
-export const deleteUser = (uid) => {
-  const index = users.findIndex((u) => u._id === uid);
-  users.splice(index, 1);
-  return { status: "ok" };
-};
+export const findUserByUsername = (username) =>
+  usersModel.findOne({ username });
+
+export const findUserByCredentials = (username, password) =>
+  usersModel.findOne({ username, password });
+
+export const createUser = (user) => usersModel.create(user);
+export const updateUser = (id, user) =>
+
+  usersModel.updateOne({ _id: id }, { $set: user });
+export const deleteUser = (id) => usersModel.deleteOne({ _id: id });
---
+++ controllers/users/users-model.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+import usersSchema from "./users-schema.js";
+
+
+const usersModel = mongoose.model("Users", usersSchema);
+export default usersModel;
